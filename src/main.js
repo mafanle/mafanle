67011df5f789a496f6dd6e98b8cfde2c4ea65bf8
yulei <1048132147@qq.com>
2020-05-06 13:17:51 +0800

    前后端数据交互测试成功

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://127.0.0.1:4000';
+Vue.prototype.axios = axios
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <button @click="hello">点击</button>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  methods:{
+    hello(){
+      this.axios.get('/hello').then(res=>{
+        var p=res.data
+        console.log(p);
+        
+      })
+    }
   }
 }
 </script>
