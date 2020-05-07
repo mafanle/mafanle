6c593355eb5cd08b608fcc1542a08843378b6aac
mafanle <yl1048132147@163.com>
2020-05-07 13:06:16 +0800

    登录页面完善

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '../views/Index.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +8,7 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Index
   },
   {
     path: '/about',
@@ -17,6 +17,22 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="content">
+    <div>这里是登录页</div>
+    <div class="login">
+      <h1>登录</h1>
+      <div class="txt">
+        <input type="text" v-model="username" placeholder="账号/手机号/用户名" />
+        <span></span>
+      </div>
+      <div class="txt">
+        <input type="password" v-model="password"  placeholder="密码
+      " />
+        <span></span>
+      </div>
+      <input type="submit" name value="ENTER" @click="login" />
+      <div class="hello">
+        没有账号?
+        <a href="#">立即注册</a>
+      </div>
+    </div>
+    <div v-show="xianshi">你妈死了</div>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            username:12345,
+            password:123,
+            xianshi:false
+        }
+    },
+    methods:{
+        login(){
+            this.axios.get('/hello').then(
+                res=>{
+                     if (res.data.code==1) {
+                         this.$router.push('/')
+                     }else{
+                         this.xianshi = true
+                     }
+                }
+            )
+           
+        }
+    }
+}
+</script>
+<style  scoped>
+
+.content {
+  min-height: 100vh;
+  background-image: linear-gradient(120deg, #4b7bec, #fc5c65);
+}
+.login {
+  background-color: white;
+  width: 350px;
+  height: 560px;
+  padding: 40px 40px;
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.login h1 {
+  text-align: center;
+  margin-bottom: 55px;
+  font-size: 30px;
+}
+
+.txt {
+  border-bottom: 2px solid #747d8c;
+  position: relative;
+  margin: 60px 0;
+}
+
+.txt input {
+  font-size: 20px;
+  color: #2c3e50;
+  width: 100%;
+  border: none;
+  outline: none;
+  background: none;
+  padding: 0 10px;
+  height: 50px;
+}
+input {
+  display: block;
+  width: 100%;
+  height: 40px;
+  border-radius: 25px;
+  border: none;
+  background: linear-gradient(120deg, #4b7bec, #fc5c65);
+  background-size: 200%;
+  color: #fff;
+}
+input:hover {
+  background-position: right;
+}
+.hello {
+  text-align: center;
+  margin-top: 90px;
+}
+
+.focus + span::before {
+  top: -5px;
+}
+
+.focus + span::after {
+  width: 100%;
+}
+</style>
