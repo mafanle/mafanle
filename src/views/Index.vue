d1494141de652b08f30b67cf1174dc94ddcda369
mafanle <yl1048132147@163.com>
2020-05-10 13:01:22 +0800

    注册模块初步完成

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,19 +1,30 @@
 <template>
- <button @click="hello">点击</button>
+  <div class="imgback">
+    <button @click="hello" class="hello">点击</button>
+
+    <el-button class="hello">你好</el-button>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  methods:{
-    hello(){
-      this.axios.get('/hello').then(res=>{
-        var p=res.data
+  methods: {
+    hello() {
+      this.axios.get("/hello").then(res => {
+        var p = res.data;
         console.log(p);
-        
-      })
+      });
     }
   }
-}
+};
 </script>
+<style scoped>
+ .hello{
+  padding: 0;
+  width: 100px;
+  height: 40px;
+ }
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="backimg" id="content">
+    <div class="reg">
+      <div class="text">
+        <span>注册</span>
+      </div>
+      <div>
+        <div class="inp">
+          <input v-model="username" type="text"  placeholder="请输入账号"/>
+        </div>
+        <div class="inp">
+          <input v-model="pwd" type="password"  placeholder="请输入密码"/>
+        </div>
+        <div class="inp">
+          <input v-model="pwds" type="password" placeholder="重复密码"/>
+        </div>
+        <div class="inp">
+          <el-button type="primary" round class="mybutton" @click="reg">注册</el-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#content {
+  position: relative;
+  animation: mycontent 1s;
+  background: url("/images/backimg.jpg") no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+}
+.text{
+  margin-top: 40px;
+}
+.text span{
+  font-size:xx-large;
+  color: aliceblue;
+}
+
+.reg {
+  border-radius: 8px;
+  width: 500px;
+  height: 350px;
+  background: rgba(00, 00, 00, 0.3);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.inp {
+  margin-top: 30px;
+  text-align: center;
+}
+.inp input  {
+  border: none;
+  height: 30px;
+  width: 70%;
+  padding-left: 5px;
+  border-radius: 4px;
+}
+.mybutton{
+  padding: 10px 160px;
+}
+
+</style>
+<script>
+export default {
+  data(){
+    return{
+      username:'123',
+      pwd:'123',
+      pwds:'123'
+    }
+  },
+  methods: {
+    menu() {
+      this.scroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (this.scroll > 0) {
+        console.log(this.scroll);
+        this.list = "listshow";
+      } else {
+        this.list = "listnone";
+      }
+    },
+    reg(){
+      var that = this      
+      this.axios.post('/reg','username='+that.username+'&pwd='+that.pwd).then(res=>{
+        console.log(res);
+      })
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.menu);
+    var $content = document.getElementById("content");
+    $content.style.height = document.documentElement.clientHeight + "px";
+    this.yidong = "contentnone";
+  }
+};
+</script>
